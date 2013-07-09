1e42e703d1b5d0a5280a7bb1295f4e5c73f0e39e
Shailen Tuli <shailen.tuli@gmail.com>
2013-07-09 15:39:22 -0700

    Storing all moves

--- web/doodle.dart
+++ web/doodle.dart
@@ -23,16 +23,18 @@ class Doodle {
   int lastX, lastY, nextX, nextY;
   bool draw = false;
   var subscription;
+  DataModel dataModel;
+  List temp = [];
 
   Doodle(this.canvas) {
     canvas.width = canvas.clientWidth;
     canvas.height = canvas.clientHeight;
 
     ctx = canvas.getContext('2d');
-    ctx.fillStyle = '#333';
     ctx.lineWidth = thicknessMap[selectedThickness];
     ctx.lineCap = 'round';
     ctx.strokeStyle = colorsMap[selectedColor];
+    dataModel = new DataModel();
   }
 
   void begin() {
@@ -46,6 +48,8 @@ class Doodle {
       subscription = canvas.onMouseMove.listen(mouseMoveCallback);
       canvas.onMouseUp.listen(mouseUpCallback);
       draw = true;
+      dataModel.paths.add(new PathData(lastX, lastY, nextX, nextY, ctx.strokeStyle, ctx.lineWidth, true));
+      drawLine();
     });
   }
 
@@ -54,6 +58,7 @@ class Doodle {
     e.stopPropagation();
     nextX = e.client.x;
     nextY = e.client.y;
+    dataModel.paths.add(new PathData(lastX, lastY, nextX, nextY, ctx.strokeStyle, ctx.lineWidth));
     drawLine();
   }
 
@@ -62,6 +67,11 @@ class Doodle {
     e.stopPropagation();
     subscription.cancel();
     draw = false;
+  }
+
+  clearCanvas() {
+    canvas.width = canvas.width;
+    print(identical(canvas, ctx.canvas));
   }
 
   drawLine() {
@@ -73,8 +83,24 @@ class Doodle {
 
     lastX = nextX;
     lastY = nextY;
+  }
 
-    // new Timer(const Duration(milliseconds: 10), drawLine);
+  drawEverything() {
+    if (!draw) return;
+
+    clearCanvas();
+    for (var i = 0; i < dataModel.paths.length; i++) {
+      ctx.lineWidth = dataModel.paths[i].lineWidth;
+      ctx.strokeStyle = dataModel.paths[i].strokeStyle;
+      _drawLine(dataModel.paths[i].lastX,
+                dataModel.paths[i].lastY,
+                dataModel.paths[i].nextX,
+                dataModel.paths[i].nextY);
+      lastX = nextX;
+      lastY = nextY;
+
+    }
+    print(dataModel.paths);
   }
 
   _drawLine(lastX, lastY, nextX, nextY) {
@@ -86,13 +112,23 @@ class Doodle {
   }
 }
 
+class DataModel {
+  List<PathData> paths = [];
+}
+
+class PathData {
+  num lastX, lastY, nextX, nextY;
+  String strokeStyle;
+  num lineWidth;
+  bool onMouseDown;
+
+  PathData(this.lastX, this.lastY, this.nextX, this.nextY,
+       this.strokeStyle, this.lineWidth, [this.onMouseDown = false]);
+
+  String toString() => '[$lastX, $lastY, $nextX, $nextY, $strokeStyle, $lineWidth, $onMouseDown]';
+}
 
 void main() {
   var doodle = new Doodle(query('canvas'));
-  window.console.log(doodle.ctx);
   doodle.begin();
-
-  queryAll('option').forEach((option) {
-    option.onSelect.listen((event) => window.console.log(event.target.value));
-  });
 }
